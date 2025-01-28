f65c1db8154b68c4885045aa404c27bb2ee964e2
angelegrd <angele.douet@edu.esiee.fr>
2025-01-28 11:54:43 +0100

    get et set piaice

--- src/Pieces/Piece.hpp
+++ src/Pieces/Piece.hpp
@@ -5,6 +5,14 @@ public:
     virtual ~Piece() = default;
     void                move();
     virtual std::string label() = 0;
+    bool                getCamp()
+    {
+        return camp;
+    };
+    bool setCamp()
+    {
+        // à voir
+    }
 
 private:
     int  id{};
