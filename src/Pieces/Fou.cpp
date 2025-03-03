baff4b0c454c7c3227ba97bd0c5a3622be2688e9
Nolan <bourrel.nolan@gmail.com>
2025-03-04 00:02:55 +0100

    correction déplacements + deplacement roi

--- src/Pieces/Fou.cpp
+++ src/Pieces/Fou.cpp
@@ -37,21 +37,21 @@ std::vector<Position> Fou::list_all_possible_moves(Board const& board, Position 
     }
     int      j{1};
     Position Next_position2 = {.x = current_position.x - j, .y = current_position.y - j};
-    while (is_move_valid(board, (Next_position), getColor()))
+    while (is_move_valid(board, (Next_position2), getColor()))
     {
         add_move_if_valid(Next_position2, possible_moves, board, getColor());
         i++;
     }
     int      k{1};
     Position Next_position3 = {.x = current_position.x + k, .y = current_position.y - k};
-    while (is_move_valid(board, (Next_position), getColor()))
+    while (is_move_valid(board, (Next_position3), getColor()))
     {
         add_move_if_valid(Next_position3, possible_moves, board, getColor());
         i++;
     }
     int      l{1};
     Position Next_position4 = {.x = current_position.x - l, .y = current_position.y + l};
-    while (is_move_valid(board, (Next_position), getColor()))
+    while (is_move_valid(board, (Next_position4), getColor()))
     {
         add_move_if_valid(Next_position4, possible_moves, board, getColor());
         i++;
