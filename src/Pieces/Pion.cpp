e9e93007933c65a50d7df89bc329be9e78d9aa30
Nolan <bourrel.nolan@gmail.com>
2025-03-04 00:08:18 +0100

    début de mouvement pion

--- src/Pieces/Pion.cpp
+++ src/Pieces/Pion.cpp
@@ -1,6 +1,8 @@
 #include "Pion.hpp"
 #include <iostream>
 #include "Board.hpp"
+#include "Piece.hpp"
+#include "utils.hpp"
 
 static void move();
 
@@ -23,4 +25,21 @@ Pion::Pion(Color color, std::string label)
 {
     this->setColor(color);
     this->setLabel(label);
+};
+
+std::vector<Position> Pion::list_all_possible_moves(Board const& board, Position current_position)
+{
+    std::vector<Position> possible_moves{};
+    Position              Next_position = {.x = current_position.x, .y = current_position.y + 1};
+    if (is_move_valid(board, (Next_position), getColor()))
+    {
+        add_move_if_valid(Next_position, possible_moves, board, getColor());
+        Position Next_position2 = {.x = current_position.x, .y = current_position.y + 2};
+        if (is_move_valid(board, (Next_position2), getColor()))
+        {
+            add_move_if_valid(Next_position2, possible_moves, board, getColor());
+        }
+    }
+
+    return possible_moves;
 };
