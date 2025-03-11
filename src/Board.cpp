6b63fd2552e28c3de395548cc0c6190c65e8d0ab
Nolan <bourrel.nolan@gmail.com>
2025-03-11 16:56:56 +0100

    EN PASSANT CA MARCHE SA MERE JE SUIS TROP KON TAN

--- src/Board.cpp
+++ src/Board.cpp
@@ -112,7 +112,7 @@ void Board::handleClick(int i, int j)
 
             else
             {
-                if (pieceMap[old_x][old_y]->label() == "P" && pion_pour_prise_en_passant.has_value() && pion_pour_prise_en_passant.value().x + 1 == i && pion_pour_prise_en_passant.value().y == j)
+                if (pieceMap[old_x][old_y]->label() == "P" && pion_pour_prise_en_passant.has_value() && ((pion_pour_prise_en_passant.value().x + 1 == i && pieceMap[old_x][old_y]->getColor() == Color::noir) || (pion_pour_prise_en_passant.value().x - 1 == i && pieceMap[old_x][old_y]->getColor() == Color::blanc)) && pion_pour_prise_en_passant.value().y == j)
                 {
                     pieceMap[pion_pour_prise_en_passant.value().x][pion_pour_prise_en_passant.value().y] = nullptr;
                 }
@@ -128,7 +128,7 @@ void Board::handleClick(int i, int j)
                     cimetiere_piece_blanche.push_back(std::move(pieceMap[i][j]));
             }
             // DANS LA LIGNE DU DESSOUS GERER L'AUTRE DIRECTION DU EN PASSANT DONC - 1 POUR L'AUTRE COULEUR
-            if (pieceMap[old_x][old_y]->label() == "P" && pion_pour_prise_en_passant.has_value() && pion_pour_prise_en_passant.value().x + 1 == i && pion_pour_prise_en_passant.value().y == j)
+            if (pieceMap[old_x][old_y]->label() == "P" && pion_pour_prise_en_passant.has_value() && ((pion_pour_prise_en_passant.value().x + 1 == i && pieceMap[old_x][old_y]->getColor() == Color::noir) || (pion_pour_prise_en_passant.value().x - 1 == i && pieceMap[old_x][old_y]->getColor() == Color::blanc)) && pion_pour_prise_en_passant.value().y == j)
             {
                 pieceMap[pion_pour_prise_en_passant.value().x][pion_pour_prise_en_passant.value().y] = nullptr;
             }
