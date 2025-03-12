f7a4664f249ccea7c95dd485c2c33a6781c3c88c
Nolan <bourrel.nolan@gmail.com>
2025-03-12 11:14:20 +0100

    Changement de lib d'enguerrand et jules appliqué

--- src/Board.cpp
+++ src/Board.cpp
@@ -25,7 +25,9 @@ Board::Board()
           {std::make_unique<Tour>(Color::blanc, "T"), std::make_unique<Cavalier>(Color::blanc, "C"), std::make_unique<Fou>(Color::blanc, "F"), std::make_unique<Reine>(Color::blanc, "Q"), std::make_unique<Roi>(Color::blanc, "K"), std::make_unique<Fou>(Color::blanc, "F"), std::make_unique<Cavalier>(Color::blanc, "C"), std::make_unique<Tour>(Color::blanc, "T")}
           // commentaire pour l'alignement de ces fucking lignes
       }}
-{}
+{
+    initializeBoard();
+}
 
 void Board::resetBoard()
 {
@@ -164,18 +166,27 @@ void Board::checkGameOver()
 void Board::draw()
 {
     quick_imgui::loop(
-        "Chess",
-        /* init: */ [&]() { initializeBoard(); },
-        /* loop: */
-        [&]() {
-            ImGui::Begin("Plateau");
-            ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+        "Quick ImGui",
+        {
+            .init = []() { std::cout << "Init\n"; },
+            .loop = [this]() { 
+                ImGui::ShowDemoWindow();
 
-            drawBoard();
-            checkGameOver();
+                ImGui::Begin("Plateau");
+                ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
 
-            ImGui::PopStyleVar();
-            ImGui::End();
+                drawBoard();
+                checkGameOver();
+
+                ImGui::PopStyleVar();
+                ImGui::End(); },
+            // .key_callback             = [](int key, int scancode, int action, int mods) { std::cout << "Key: " << key << " Scancode: " << scancode
+            //                                                                             << " Action: " << action << " Mods: " << mods << '\n'; },
+            // .mouse_button_callback    = [](int button, int action, int mods) { std::cout << "Button: " << button << " Action: " << action
+            //                                                                           << " Mods: " << mods << '\n'; },
+            // .cursor_position_callback = [](double xpos, double ypos) { std::cout << "Position: " << xpos << ' ' << ypos << '\n'; },
+            // .scroll_callback          = [](double xoffset, double yoffset) { std::cout << "Scroll: " << xoffset << ' ' << yoffset << '\n'; },
+            // .window_size_callback     = [](int width, int height) { std::cout << "Resized: " << width << ' ' << height << '\n'; },
         }
     );
 }
